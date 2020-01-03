8086c830ff8094b7c164f68b5e86adf43b871a29
Jocelyn Chang <jocelync1094@gmail.com>
2020-01-02 21:01:34 -0800

    db folder created and database created

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Double Double Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Chicken Sandwich');
+INSERT INTO burgers (name, devour) VALUES ('Quarter Pounder', false);
+INSERT INTO burgers (name, devour) VALUES ('Big Mac', false);
+INSERT INTO burgers (name) VALUES ('Jumbo Jack Cheese Burger', false);
